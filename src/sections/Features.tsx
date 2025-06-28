363e1cfc673d348f18c53ca661ff902154e19fe9
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-06-28 14:02:00 +0530

    images added

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,5 +1,11 @@
+import FeatureCard from "@/components/featureCard";
 import Tag from "@/components/Tags";
-import { div } from "framer-motion/client";
+import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg'
+import avatar2 from '@/assets/images/avatar-florence-shaw.jpg'
+import avatar3 from '@/assets/images/avatar-lula-meyers.jpg'
+import avatar4 from '@/assets/images/avatar-owen-garcia.jpg'
+import Image from "next/image";
+
 
 const features = [
     "Asset Library",
@@ -12,44 +18,56 @@ const features = [
 ];
 
 export default function Features() {
-    return <section>
+    return <section className="py-24">
          <div className="container">
-            <Tag className="flex justify-center w-fit items-center">features</Tag>
-            <h2>Where power meets <span>simplicity</span> </h2>
+            <div className="flex justify-center items-center">
+            <Tag>features</Tag>
+            </div>
+            <h2 className="text-center text-6xl font-medium mt-6 ">Where power meets <span className="text-lime-400">simplicity</span> </h2>
              <div>
                 {/* first feature */}
-                <div>
-                    <div></div>
-                    <div>
-                    <h3>Real Time Collaboration</h3>
-                    <p>Work Together seamlessly with conflict-free team editing  </p>
-                    </div>
+                <div className="mt-12 grid grid-cols-1 gap-8">
+                   <div>
+                   
+                   <FeatureCard title="Real time Collaboration" description="Work together seemlessly with conflict-free team editing " children={
+                    <div className="flex justify-center items-center py-14 gap-4  ">
+                        <div>
+                        <Image alt="image" src={avatar1}/>
+                        </div>
+                        <div>
+                        <Image alt="image" src={avatar2}/>
+                        </div>
+                        <div>
+                        <Image alt="image" src={avatar3}/>
+                        </div>
+                   </div>}/>
+                     
+                    </div> 
+                   
+               <div>
+                
+                </div>
                 </div>
                  {/* second feature */}
-                <div>
-                    <div></div>
-                    <div>
-                    <h3>Interactive Protyping</h3>
-                    <p>eEngage your clients with a prototype that reacts to user actions </p>
-                    </div>
+                <div className="mt-12 grid grid-cols-1 gap-8">
+               
+                    <FeatureCard title="Interactive Protyping" description="Engage your clients with a prototype that reacts to user actions"/>
+                 
                 </div>
                 {/* third feature */}
-                <div>
-                    <div></div>
-                    <div>
-                    <h3>Keyword Quick Actions</h3>
-                    <p>Powerfull commands to help you create designs more quickly </p>
-                    </div>
+                <div className="mt-12 grid grid-cols-1 gap-8">
+
+                    <FeatureCard title="Keyword Quick Actions" description="Powerfull commands to help you create designs more quickly  "/>
+
                 </div>
              </div>
              <div>
-                {features.map(feature)=>(
-                    <div key={feature}>
-                         <span></span>
-                         <span>{feature}</span>
-                    </div>
-                    
-                ))}
+              {features.map((feature)=>(
+                <div key={feature}>
+                    <span></span>
+                    <span>{feature}</span>
+                </div>
+            ))}
              </div>
         </div>
     </section>;
